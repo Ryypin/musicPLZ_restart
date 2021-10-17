3a984effc3aaa273a8f156399b6cb64a198e3257
Ryypin <harris.harris.steven@hotmail.com>
2021-10-17 16:50:54 +0200

    Fin HEADER + Début slider

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,8 +3,9 @@
 let spaceInput = 0;
 let inputCount = 0;
 let timer;
-let video = document.querySelector(".header__video");
+let video = document.querySelector(".main__video");
 let logo = document.querySelector("#logo");
+let mainMobile = document.querySelector(".main");
 
 
 // <---------- SPACE BAR ANIMATION ---------->
@@ -12,27 +13,46 @@ let logo = document.querySelector("#logo");
 document.body.onkeydown = function(e){
     if(e.keyCode != 32){
         video.pause();
+    } else if(inputCount == 12){
+        e.preventDefault();
+        clearTimeout(timer);
+        video.play();
+        showEnd();
+        logo.style.transform = `scale(0)`;
     } else if(e.keyCode == 32 && spaceInput < 2){
         spaceClick();
         e.preventDefault();
         spaceInput++;
         inputCount++;
-    } else if(inputCount == 12){
-        e.preventDefault();
-        clearTimeout(timer);
-        video.play();
-        logo.style.transform = `scale(0)`;
     } else {
         e.preventDefault();
         spaceClick();
     }
 }
+mainMobile.addEventListener("touchend", (e) => {
+    if(inputCount == 12){
+        e.preventDefault();
+        clearTimeout(timer);
+        video.play();
+        showEnd();
+        logo.style.transform = `scale(0)`;
+    } else if(spaceInput < 2){
+        spaceClick();
+        e.preventDefault();
+        spaceInput++;
+        inputCount++;
+    } else {
+        e.preventDefault();
+        spaceClick();
+    }
+})
 
 function Timer(){
     clearTimeout(timer);
     video.play();
     spaceInput = 0;
     timer = setTimeout(function stop(){
+        show();
         spaceInput = 0;
         video.pause();
     }, 3000);
@@ -42,6 +62,8 @@ function spaceClick(){
     if(spaceInput == 2){
         Timer();
         animation();
+        endOF();
+        showEnd();
         spaceInput = 0;
     } else {
         animation();
@@ -58,6 +80,21 @@ function animation(){
         duration: 100,
         iterations: 1
       });
+}
+
+function endOF(){
+    let mainText = document.querySelector(".main__text");
+    mainText.classList.add("hidden");
+}
+
+function show(){
+    let mainShow = document.querySelector(".main__text--show");
+    mainShow.classList.remove("hidden");
+}
+
+function showEnd(){
+    let mainShow = document.querySelector(".main__text--show");
+    mainShow.classList.add("hidden");
 }
 
 // <---------- BURGER MENU ---------->
